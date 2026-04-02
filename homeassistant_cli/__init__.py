32e531bfc72935e1fb988ccc62f1cab7192331e2
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-04-02 21:42:30 +0200

    fix: rename PyPI package to homeassistantcli

--- homeassistant_cli/__init__.py
+++ homeassistant_cli/__init__.py
@@ -4,7 +4,7 @@ homeassistant-cli: Agent-friendly CLI for Home Assistant.
 
 try:
     from importlib.metadata import version
-    __version__ = version("ha-cli")
+    __version__ = version("homeassistantcli")
 except ImportError:
     from importlib_metadata import version
-    __version__ = version("ha-cli")
+    __version__ = version("homeassistantcli")
